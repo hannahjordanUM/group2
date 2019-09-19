0a8735e58a7bbd8149be587efc189e731c402647
Beau Goldberg <beaug1999@hotmail.com>
2019-09-19 14:58:50 -0600

    updated again

--- group.c
+++ group.c
@@ -10,9 +10,10 @@ void TimGarner () {
 
 void BeauGoldberg () {
  printf("Name: Beau Goldberg\n");
- printf("Year: Junior");
- printf("Major: Computational Physics");
- printf("Hobbies: Sports, Gaming");
+ printf("Year: Junior\n");
+ printf("Major: Computational Physics\n");
+ printf("Hobbies: Sports, Gaming\n");
+ printf("Hometown: Polson, MT\n");
     
 }
 
