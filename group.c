07dc820c7d68b3776759e3fe7fdd288c1972d106
Hannah Jordan <53317611+hannahjordanUM@users.noreply.github.com>
2019-09-19 14:40:55 -0600

    Add files via upload

---
+++ group.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+void RandyFlores () {
+    
+}
+
+void TimGarner () {
+    
+}
+
+void BeauGoldberg () {
+    
+}
+
+void HannahJordan () {
+    
+}
+
+int main() {
+    printf("This is group 2");
+    printf("Members are: Randy, Tim, Beau, and Hannah");
+    
+    
+    
+    return 0;
+}
